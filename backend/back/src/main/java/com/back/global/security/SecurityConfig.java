9c487a9e57f3c6121101a572df1784d283c86574
zunobk <junho4560@naver.com>
2026-01-20 12:29:58 +0900

    Feat: 로그인 성공 시 JWT 발급(JwtProvider / JwtProperties / SecurityConfig)

--- backend/back/src/main/java/com/back/global/security/SecurityConfig.java
+++ backend/back/src/main/java/com/back/global/security/SecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.back.global.security.jwt.JwtAuthenticationFilter;
 import com.back.global.security.jwt.JwtProperties;
 
 import lombok.RequiredArgsConstructor;
@@ -16,18 +18,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableConfigurationProperties(JwtProperties.class)
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/favicon.ico")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(auth -> auth
+                        // 인증 없이 허용할 엔드포인트
+                        .requestMatchers("/favicon.ico")
                         .permitAll()
                         .requestMatchers("/h2-console/**")
                         .permitAll()
-                        .requestMatchers("/**")
+                        .requestMatchers("/api/v1/member/join")
                         .permitAll()
+                        .requestMatchers("/api/v1/member/login")
+                        .permitAll()
+
+                        // 나머지는 인증 필요
                         .anyRequest()
                         .authenticated())
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .csrf(AbstractHttpConfigurer::disable);
+                // JWT 필터를 Spring Security 필터 체인에 등록
+                // UsernamePasswordAuthenticationFilter 이전에 실행되게 두는 게 일반적
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
