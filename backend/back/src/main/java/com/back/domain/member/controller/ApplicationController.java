4aa05e4c73932b95f1ef34028dcddf87b0a1fbaf
김효신 <kimhyoshin67@gachon.ac.kr>
2026-01-20 10:29:25 +0900

    Feat: Controller 응답 구조 작성

--- backend/back/src/main/java/com/back/domain/member/controller/ApplicationController.java
+++ backend/back/src/main/java/com/back/domain/member/controller/ApplicationController.java
@@ -1,13 +1,54 @@
 package com.back.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.back.domain.member.dto.AddApplicationResponseDto;
+import com.back.domain.member.entity.Application;
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.ApplicationService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/member")
+@RequiredArgsConstructor
 public class ApplicationController {
 
-    private ApplicationService applicationService;
+    private final ApplicationService applicationService;
+
+    @GetMapping("/welfare-applications")
+    public ResponseEntity<List<Application>> getApplicationList() {
+        Member member = new Member(); // TODO: jwt에서 멤버 추출
+
+        applicationService.getApplicationList(member);
+
+        return ResponseEntity.ok(applicationService.getApplicationList(member));
+    }
+
+    @PostMapping("/welfare-application/{id}")
+    public ResponseEntity<AddApplicationResponseDto> addApplication(@PathVariable Integer id) {
+        Member member = new Member(); // TODO: jwt에서 멤버 추출
+
+        if (member == null) {
+            AddApplicationResponseDto addApplicationResponseDto =
+                    new AddApplicationResponseDto(HttpStatus.BAD_REQUEST.value(), "로그인 후 이용해주세요");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(addApplicationResponseDto);
+        }
+
+        Application application = applicationService.addApplication(member, id);
+
+        if (application != null) {
+            AddApplicationResponseDto addApplicationResponseDto =
+                    new AddApplicationResponseDto(HttpStatus.OK.value(), "저장되었습니다!");
+            return ResponseEntity.status(HttpStatus.OK).body(addApplicationResponseDto);
+        } else {
+            AddApplicationResponseDto addApplicationResponseDto =
+                    new AddApplicationResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "저장 중 문제가 발생했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(addApplicationResponseDto);
+        }
+    }
 }
