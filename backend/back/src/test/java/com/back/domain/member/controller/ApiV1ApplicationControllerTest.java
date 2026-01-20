e914a7966faa9d4a71684eaf64f4a03affde37d2
김효신 <kimhyoshin67@gachon.ac.kr>
2026-01-20 11:41:13 +0900

    Feat : 신청 정책 삭제 기능 구현

---
+++ backend/back/src/test/java/com/back/domain/member/controller/ApiV1ApplicationControllerTest.java
@@ -0,0 +1,19 @@
+package com.back.domain.member.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1ApplicationControllerTest {
+
+    // TODO: 1. 신청 내역 추가하기
+
+    // TODO: 2. 신청 내역 조회하기
+
+    // TODO: 3. 신청 내역 삭제하기
+}
