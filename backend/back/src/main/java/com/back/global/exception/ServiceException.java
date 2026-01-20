86f8df31786a9f43f687aa227dab882a601060bb
zunobk <junho4560@naver.com>
2026-01-20 09:36:18 +0900

    Feat: 전역 ServiceException 처리용 GlobalExceptionHandler 추가

--- backend/back/src/main/java/com/back/global/exception/ServiceException.java
+++ backend/back/src/main/java/com/back/global/exception/ServiceException.java
@@ -1,5 +1,8 @@
 package com.back.global.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ServiceException extends RuntimeException {
     private final String resultCode;
     private final String msg;
