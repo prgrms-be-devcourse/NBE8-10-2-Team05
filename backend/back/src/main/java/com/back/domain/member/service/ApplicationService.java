57724f9a3e6435464ac0e612929d9f14564e0029
김효신 <kimhyoshin67@gachon.ac.kr>
2026-01-20 10:41:06 +0900

    Feat: application 추가 기능 구현

--- backend/back/src/main/java/com/back/domain/member/service/ApplicationService.java
+++ backend/back/src/main/java/com/back/domain/member/service/ApplicationService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.back.domain.member.entity.Application;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.ApplicationRepository;
+import com.back.domain.welfare.policy.entity.Policy;
+import com.back.domain.welfare.policy.repository.PolicyRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,11 +18,25 @@ public class ApplicationService {
 
     private final ApplicationRepository applicationRepository;
 
+    private final PolicyRepository policyRepository;
+
     public List<Application> getApplicationList(Member member) {
         return applicationRepository.getApplicationsById(member.getId());
     }
 
-    public Application addApplication(Member member, int id) {}
+    public Application addApplication(Member member, int policyId) {
+        Application application = new Application();
 
-    public boolean deleteApplication(Application application) {}
+        Policy policy = policyRepository.findPolicyById(policyId);
+        application.setPolicy(policy);
+        application.setApplicant(member);
+
+        applicationRepository.save(application);
+        return application;
+    }
+
+    public boolean deleteApplication(Application application) {
+        applicationRepository.delete(application);
+        return true;
+    }
 }
