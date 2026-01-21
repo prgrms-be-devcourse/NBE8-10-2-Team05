e0297960e70c49b8a9c9693fe08748f751d3cfdd
suyeonTech <wsy-ofc@naver.com>
2026-01-21 09:17:11 +0900

    Refactor: PolicyController에 RequestMapping 적용

--- backend/back/src/main/java/com/back/domain/welfare/policy/controller/PolicyController.java
+++ backend/back/src/main/java/com/back/domain/welfare/policy/controller/PolicyController.java
@@ -3,17 +3,22 @@ package com.back.domain.welfare.policy.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.back.domain.welfare.policy.dto.PolicyRequestDto;
 import com.back.domain.welfare.policy.dto.PolicyResponseDto;
 import com.back.domain.welfare.policy.service.PolicyService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api/v1/welfare/policy")
+@RequiredArgsConstructor
 public class PolicyController {
     PolicyService policyService;
 
-    @GetMapping("/api/v1/welfare/policy/search")
+    @GetMapping("/search")
     public List<PolicyResponseDto> search(PolicyRequestDto policyRequestDto) {
         return policyService.search(policyRequestDto);
     }
