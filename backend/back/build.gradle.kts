debcc6d201d737e7a677e7a4d5d072a4a78652e1
김효신 <kimhyoshin67@gachon.ac.kr>
2026-01-23 16:34:13 +0900

    Fix : dependency 버전 깨짐 + 하드코딩 수정

--- backend/back/build.gradle.kts
+++ backend/back/build.gradle.kts
@@ -112,9 +112,10 @@ dependencies {
     implementation("org.jsoup:jsoup:1.17.2") // 웹 크롤링을 위해 Jsoup 라이브러리 추가
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    //Elastic Search
-    implementation ("co.elastic.clients:elasticsearch-java:8.11.3")
-    implementation ("org.elasticsearch.client:elasticsearch-rest-client:8.12.")
+    // Elasticsearch (Java API Client + Low Level Rest Client)
+    // 버전은 반드시 맞춰서 사용하세요.
+    implementation("co.elastic.clients:elasticsearch-java:8.11.3")
+    implementation("org.elasticsearch.client:elasticsearch-rest-client:8.11.3")
     implementation ("org.apache.httpcomponents.client5:httpclient5")
 
 }
