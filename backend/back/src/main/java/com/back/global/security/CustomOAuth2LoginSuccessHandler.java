c6edee5caa12a3fc83fc2d23cbc5e150bc0b44b2
zunobk <junho4560@naver.com>
2026-01-26 14:19:25 +0900

    Feat: CustomOAuth2LoginSuccessHandler 추가하여 소셜로그인 성공시 프론트로 리다이렉트

---
+++ backend/back/src/main/java/com/back/global/security/CustomOAuth2LoginSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.back.global.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        response.sendRedirect("http://localhost:3000");
+    }
+}
