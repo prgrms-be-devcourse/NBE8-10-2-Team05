8cf34e1bc5b489607275e841948a52f13d8307f1
김효신 <kimhyoshin67@gachon.ac.kr>
2026-01-27 14:28:06 +0900

    Refactor : Policy Apply 이름 변경 + Record type으로 변경

--- backend/back/src/main/java/com/back/domain/member/bookmark/dto/BookmarkPolicyResponseDto.java
+++ backend/back/src/main/java/com/back/domain/member/bookmark/dto/BookmarkPolicyResponseDto.java
@@ -4,19 +4,4 @@ import java.util.List;
 
 import com.back.domain.welfare.policy.entity.Policy;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class BookmarkPolicyResponseDto {
-    private int code;
-    private String message;
-    private List<Policy> policies;
-
-    public BookmarkPolicyResponseDto(int code, String message, List<Policy> policies) {
-        this.code = code;
-        this.message = message;
-        this.policies = policies;
-    }
-}
+public record BookmarkPolicyResponseDto(int code, String message, List<Policy> policies) {}
