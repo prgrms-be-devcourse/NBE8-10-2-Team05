8530d4d9c8658091db4d35e18c3dcdbbcc48957d
lonelylonelylonely <lonelylonelylonely@example.com>
2026-01-28 17:21:31 +0900

    fix : CenterController - @Controller -> @RestController로 수정

--- backend/back/src/main/java/com/back/domain/welfare/center/center/controller/CenterController.java
+++ backend/back/src/main/java/com/back/domain/welfare/center/center/controller/CenterController.java
@@ -2,10 +2,10 @@ package com.back.domain.welfare.center.center.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.back.domain.welfare.center.center.dto.CenterSearchResponseDto;
 import com.back.domain.welfare.center.center.entity.Center;
@@ -13,7 +13,7 @@ import com.back.domain.welfare.center.center.service.CenterService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/welfare/center")
 @RequiredArgsConstructor
 public class CenterController {
