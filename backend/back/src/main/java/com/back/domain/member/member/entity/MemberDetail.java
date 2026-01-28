0bc2aff0549ddb3573ff1a8276e10b8c6f19ad94
lonelylonelylonely <lonelylonelylonely@example.com>
2026-01-28 14:38:13 +0900

    fix : MemberDetail의 address 수정

--- backend/back/src/main/java/com/back/domain/member/member/entity/MemberDetail.java
+++ backend/back/src/main/java/com/back/domain/member/member/entity/MemberDetail.java
@@ -69,25 +69,25 @@ public class MemberDetail {
     @Builder
     public static class Address {
         // 카카오 우편번호 검색 API 제공
-        String postcode; // 우편번호
-        String addressName; // 전체 주소
-        String sigunguCode; // 41135 시/군/구 코드
-        String bCode;
+        private String postcode; // 우편번호
+        private String addressName; // 전체 주소
+        private String sigunguCode; // 41135 시/군/구 코드
+        private String bCode;
         // 4113511000	법정동/법정리 코드
-        String roadAddress;
+        private String roadAddress;
         // 도로명주소
-        String sigungu;
+        private String sigungu;
         // 시/군/구 이름 "성남시 분당구"
-        String sido;
+        private String sido;
         // 도/시 이름 "경기"
 
         // 카카오 Local API 제공
         // 도로명 주소로 가져온다.
-        String hCode;
+        private String hCode;
         // "4514069000" 행정동 코드
-        Double latitude;
+        private Double latitude;
         // 위도
-        Double longitude;
+        private Double longitude;
         // 경도
 
         public static Address from(com.back.domain.member.geo.entity.Address dto) {
