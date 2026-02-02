ce8fd1292be4b91cf60927ae63fae4858462b4e5
gurum505 <sayhojeong505@gmail.com>
2026-02-02 16:48:49 +0900

    fix : dto getter 수정, CustomAuthenticationFilter.java 제거

--- backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateRegionDto.java
+++ backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateRegionDto.java
@@ -1,7 +1,9 @@
 package com.back.domain.welfare.estate.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class EstateRegionDto {
     private String name;
