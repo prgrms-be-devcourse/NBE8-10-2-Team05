037804df8c183c36dbafb3b819f558af043d79e5
gurum505 <sayhojeong505@gmail.com>
2026-02-25 09:44:29 +0900

    refactor: geo properties 변경

--- backend/back/src/main/java/com/back/domain/member/geo/properties/GeoApiProperties.kt
+++ backend/back/src/main/java/com/back/domain/member/geo/properties/GeoApiProperties.kt
@@ -3,5 +3,4 @@ package com.back.domain.member.geo.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "custom.api.geo")
-@JvmRecord
-data class GeoApiProperties(val key: String?, val url: String?)
+data class GeoApiProperties(val key: String, val url: String)
