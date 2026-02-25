320516a44342308484e40427726e1fdcb67df5f8
gurum505 <sayhojeong505@gmail.com>
2026-02-25 15:45:57 +0900

    refactor: global webmvc 변경

--- backend/back/src/main/java/com/back/global/webMvc/WebMvcConfig.kt
+++ backend/back/src/main/java/com/back/global/webMvc/WebMvcConfig.kt
@@ -1,17 +1,16 @@
-package com.back.global.webMvc;
+package com.back.global.webMvc
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
